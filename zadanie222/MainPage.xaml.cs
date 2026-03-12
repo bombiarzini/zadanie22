1b2f7621d5592b1a2c5899b402ddabeddf1b6b6a
bombiarzini <kacperdragon025@gmail.com>
2026-03-12 10:10:40 +0100

    Zadanie 22.2

---
+++ zadanie222/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿
+
+namespace zadanie222
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnEmailTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var email = EmailEntry.Text;
+            if (email.Contains("@") && email.Contains(".") && !string.IsNullOrWhiteSpace(email))
+            {
+
+                EmailErrorLabel.IsVisible = false;
+            }
+            else
+            {
+                EmailErrorLabel.IsVisible = true;
+
+
+            }
+
+
+            if (EmailErrorLabel.IsVisible == true || PasswordErrorLabel.IsVisible == true)
+            {
+                LoginButton.Background = Colors.Grey;
+            }
+            else
+            {
+                LoginButton.Background = Colors.Green;
+            }
+        }
+
+
+        public void OnPasswordTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var password = PasswordEntry.Text;
+            if (password.Length >= 6 && !string.IsNullOrWhiteSpace(password))
+            {
+                PasswordErrorLabel.IsVisible = false;
+            }
+            else
+            {
+                PasswordErrorLabel.IsVisible = true;
+
+            }
+
+
+            if (EmailErrorLabel.IsVisible == true || PasswordErrorLabel.IsVisible == true)
+            {
+                LoginButton.Background = Colors.Grey;
+            }
+
+            else
+            {
+                LoginButton.Background = Colors.Green;
+            }
+        }
+        public void OnLoginClicked(object sender, EventArgs e)
+        {
+            if(EmailErrorLabel.IsVisible == false && PasswordErrorLabel.IsVisible == false)
+            {
+                DisplayAlert("Login", "gicio!", "OK");
+            }
+            else
+            {
+                DisplayAlert("Login", "nie gicio!", "nie OK");
+            }
+            
+
+        }
+    }
+}
---
+++ zadanie222/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace zadanie222
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
